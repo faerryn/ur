71837774f93aa8fd0d99f71e88129f1b6d90f621
Faerryn <alexandre.liao@gmail.com>
2026-08-20 21:04:40 -0400

    tar up built distributions

--- build_distributions.sh
+++ build_distributions.sh
@@ -5,3 +5,5 @@ RELEASE=fast
 
 curl 'https://ziglang.org/download/index.json?source=ur' | jq -r '.master|keys[]' | grep -- - |
 	parallel zig build --prefix-exe-dir "$DIST_DIR"/{} --release="$RELEASE" -Dtarget={}
+
+tar -cvaf ur.tar.xz -C "zig-out/$DIST_DIR" .
