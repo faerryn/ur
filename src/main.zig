f62ccfb0965e17ecfa8f5a039677fb508c85ef41
Faerryn <alexandre.liao@gmail.com>
2026-08-27 12:08:30 -0400

    use the platform-specific PATH delimiter instead of ':'

--- src/main.zig
+++ src/main.zig
@@ -237,7 +237,7 @@ fn shim(g: ur.Global, library: *ur.Library, index: *ur.RemoteIndex, spec: ur.Spe
             errdefer g.init.gpa.free(newpath);
             // TODO: maybe use sprintf?
             std.mem.copyForwards(u8, newpath, zig_spec_path_buf[0..zig_spec_path_len]);
-            newpath[zig_spec_path_len] = ':';
+            newpath[zig_spec_path_len] = std.fs.path.delimiter;
             std.mem.copyForwards(u8, newpath[zig_spec_path_len + 1 ..], oldpath);
             try g.init.environ_map.put("PATH", newpath);
         } else |_| {}
