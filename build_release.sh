a7842327fd0aba6ec9646a7db964f78eb1ef943c
Faerryn <alexandre.liao@gmail.com>
2026-09-02 02:03:05 -0400

    cleaner release build script

---
+++ build_release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Ensure programs are available
+ensure() {
+  command -v "$1" >/dev/null 2>&1 && return 0
+  echo >&2 "$1 required"
+  return 1
+}
+
+ensure parallel || exit 1
+ensure zig || exit 1
+ensure tar || exit 1
+
+# Define variables
+RELEASE_MODE="${1:-safe}"
+
+BUILD_TARGETS="$PWD/build_targets.txt"
+DIST_DIR="dist-$RELEASE_MODE"
+OUT_DIR="$PWD/zig-out"
+
+OUT_DIST_DIR="$OUT_DIR/$DIST_DIR"
+
+# Compute platforms that zig supports and compile
+parallel --bar zig build --prefix-exe-dir "$DIST_DIR"/{} --release="$RELEASE_MODE" -Dtarget={} :::: "$BUILD_TARGETS"
+
+# Package binaries into a tarball
+tar -cvaf "ur-$RELEASE_MODE.tar.xz" -C "$OUT_DIST_DIR" .
